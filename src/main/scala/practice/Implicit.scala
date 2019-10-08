8835486f103ea94149b8546513c7559bbd39148b
Fabian Wennink <contact@fabianwennink.nl>
2019-10-08 16:26:33 +0200

    Removed the practice files

--- src/main/scala/practice/Implicit.scala
+++
@@ -1,9 +0,0 @@
-package practice
-
-object Implicit extends App {
-
-  implicit val prefix: String = "Hello "
-  def prefix(s: String)(implicit prefix: String): String = prefix ++ s
-
-  println(prefix("Fabian")) // Hello Fabian
-}
